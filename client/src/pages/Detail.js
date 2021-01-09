4b5abfc21d0da1d78f5e4045be9683767ad9643e
terahje <terahjequinn@gmail.com>
2021-01-09 14:55:27 -0700

    noticed error in console with details and home js, error fixed

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
+
+import Cart from "../components/Cart";
 import { useStoreContext } from "../utils/GlobalState";
-import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from '../assets/spinner.gif'
-import Cart from '../components/Cart';
-import { idbPromise } from "../utils/helpers";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
-} from '../utils/actions';
+} from "../utils/actions";
+import { QUERY_PRODUCTS } from "../utils/queries";
+import { idbPromise } from "../utils/helpers";
+import spinner from '../assets/spinner.gif'
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
-  const { products, cart } = state;
   const { id } = useParams();
-  
-  const [currentProduct, setCurrentProduct] = useState({})
-  
+
+  const [currentProduct, setCurrentProduct] = useState({});
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
+
+  const { products, cart } = state;
+
   useEffect(() => {
     // already in global store
     if (products.length) {
@@ -33,7 +35,7 @@ function Detail() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
-  
+
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
@@ -48,18 +50,15 @@ function Detail() {
       });
     }
   }, [products, data, loading, dispatch, id]);
-  
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
-  
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
-      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
@@ -69,10 +68,10 @@ function Detail() {
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
-      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
       idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+
+    }
   }
-}
 
   const removeFromCart = () => {
     dispatch({
@@ -80,14 +79,12 @@ function Detail() {
       _id: currentProduct._id
     });
 
-      // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
     idbPromise('cart', 'delete', { ...currentProduct });
-};
-  
+  };
 
   return (
     <>
-      {currentProduct ? (
+      {currentProduct && cart ? (
         <div className="container my-1">
           <Link to="/">
             ← Back to Products
@@ -103,6 +100,9 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
+            <button onClick={addToCart}>
+              Add to Cart
+            </button>
             <button 
               disabled={!cart.find(p => p._id === currentProduct._id)} 
               onClick={removeFromCart}
