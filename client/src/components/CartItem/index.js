cd07a7491a125da95a0bc7aea1daf1cb43656da8
terahje <terahjequinn@gmail.com>
2021-01-09 14:45:24 -0700

    added offline capabilities with indexedDb

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
+import { idbPromise } from "../../utils/helpers";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 const CartItem = ({ item }) => {
@@ -10,6 +11,8 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
             });
+        idbPromise('cart', 'delete', { ...item });
+
     };
 
     const onChange = (e) => {
@@ -20,12 +23,16 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
           });
+
+          idbPromise('cart', 'delete', { ...item });
         } else {
           dispatch({
             type: UPDATE_CART_QUANTITY,
             _id: item._id,
             purchaseQuantity: parseInt(value)
           });
+          
+          idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
       };
   return (
